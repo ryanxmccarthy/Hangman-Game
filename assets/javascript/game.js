36e6c71fa8b98a3ddbe5fca8ed321fd20d0acacc
Ryan McCarthy <ryanxmccarthy@gmail.com>
2017-09-07 00:07:00 -0400

    hid letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,42 @@
-// use .split to turn the string from your array into an array with each letter separate
 // use indexOf to see if the guessed letter is anywhere in the array
 // if it is, inject that letter into the empty array
 
 
-var words = ['Legolas', 'Gimli', 'Aragorn', 'Sauron', 'Rivendell', 'Glamdring', 'Anduril', 'Frodo', 'Nazgul', 'Mordor', 'Narsil', 'Gollum', 'Bombadil', 'Samwise', 'Manwe']
-var lettersGuessed = [];
-var guessesLeft = 13;
+// var words = ['Legolas', 'Gimli', 'Aragorn', 'Sauron', 'Rivendell', 'Glamdring', 'Anduril', 'Frodo', 'Nazgul', 'Mordor', 'Narsil', 'Gollum', 'Bombadil', 'Samwise', 'Manwe', 'Greyhame', 'Stormcrow', 'Valar', 'Maiar']
+var words = [['L', 'e', 'g', 'o', 'l', 'a', 's'], ['G', 'i', 'm', 'l', 'i']]
 var compChoice = words[Math.floor(Math.random() * words.length)];
+hideWord();
+var lettersGuessed = [];
+var guessesLeft = 10;
 document.getElementById("wordToGuess").innerHTML = compChoice;
 
 //define functions
+function hideWord() {
+	for(var i = 0; i < compChoice.length; i++) {
+		compChoice[i] = "_ ";
+	}
+}
 var addLettersGuessed = function() {  
 	document.getElementById('lettersGuessed').innerHTML = lettersGuessed.join(', ');
-};
+}
 var updateLettersGuessed = function() {
 	document.getElementById('lettersGuessed').innerHTML = lettersGuessed;
-};
+}
 var updateGuessesLeft = function() {
 	document.getElementById('guessesLeft').innerHTML = guessesLeft;
-};
+}
 var newWord = function() {
 	var compChoice = words[Math.floor(Math.random() * words.length)];
 	document.getElementById("wordToGuess").innerHTML = compChoice;
-};
+}
 var reset = function() {
-	guessesLeft = 13;
+	guessesLeft = 10;
 	lettersGuessed = [];
 	updateLettersGuessed();
 	updateGuessesLeft();
 	newWord();
-};
+	hideWord();
+}
 
 //runs when user makes a guess
 document.onkeyup = function(event) {
